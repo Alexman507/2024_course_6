4eb27dfa4d21df4e702e4e2f086c2df2fe1af5e5
NeroPC <Nerocross507@gmail.com>
2024-07-13 12:48:00 +0300

    Realize admin, urls, views for all apps. Fixed settings.py. Models for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BlogArticle
 
-# Register your models here.
+
+@admin.register(BlogArticle)
+class BlogArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body', 'created_at', 'preview', 'views_count', )
+    search_fields = ('title', )
+    list_filter = ('created_at', )
